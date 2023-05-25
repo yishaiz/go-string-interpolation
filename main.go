e056f9522323ef9a9204a0f49ec25729c44bbaeb
yishai <yishai5555@gmail.com>
2023-05-25 16:16:02 +0300

    02-refactor

--- main.go
+++ main.go
@@ -7,13 +7,27 @@ import (
 	"strings"
 )
 
+var reader *bufio.Reader
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("What is your name?")
+	reader = bufio.NewReader(os.Stdin)
+
+	userName := readString("What is your name?")
+
+	fmt.Println("Your name is", userName)
+}
+
+func prompt() {
 	fmt.Print("->")
+}
+
+func readString(s string) string {
+	fmt.Println(s)
+
+	prompt()
 
 	userInput, _ := reader.ReadString('\n')
 	userInput = strings.Replace(userInput, "\n", "", -1)
 
-	fmt.Println("Your name is", userInput)
+	return userInput
 }
