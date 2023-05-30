8f3cc23fab190c0b90b732421a8873724e635fea
yishai <yishai5555@gmail.com>
2023-05-30 18:42:14 +0300

    07-add-type-user-and-float64

--- main.go
+++ main.go
@@ -10,17 +10,28 @@ import (
 
 var reader *bufio.Reader
 
+type User struct {
+	UserName        string
+	Age             int
+	FavouriteNumber float64
+}
+
 func main() {
 	reader = bufio.NewReader(os.Stdin)
 
-	userName := readString("What is your name?")
+	var user User
 
-	age := readInt("How old are you?")
+	user.UserName = readString("What is your name?")
 
-	// fmt.Println("Your name is "+userName+", and you are", age, "years old")
+	user.Age = readInt("How old are you?")
 
-	fmt.Println(fmt.Sprintf("Your name is %s and you are %d years old", userName, age))
+	user.FavouriteNumber = readFloat("What is your favourite number?")
 
+	fmt.Println(fmt.Sprintf("Your name is %s and you are %d years old. Your favourite unmber is %.2f",
+		user.UserName,
+		user.Age,
+		user.FavouriteNumber,
+	))
 }
 
 func prompt() {
@@ -51,6 +62,25 @@ func readInt(s string) int {
 		num, err := strconv.Atoi(userInput)
 		if err != nil {
 			fmt.Println("Please enter a whole number")
+		} else {
+			return num
+		}
+	}
+}
+
+func readFloat(s string) float64 {
+	for {
+
+		fmt.Println((s))
+
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.ParseFloat(userInput, 64)
+		if err != nil {
+			fmt.Println("Please enter a number")
 		} else {
 			return num
 		}
